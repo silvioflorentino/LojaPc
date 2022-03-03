8a86d308c1e75e9fa83ef2e6fb98f09a46b40c53
Silvio Wanderson Florentino <professorsilvioflorentino@yahoo.com.br>
2022-03-03 14:18:03 -0300

    Arquivos principais

---
+++ App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { StatusBar } from 'react-native';
+
+import Rotas from './components/rotasBotao';
+import Home from './components/home';
+export default function App() {
+  return (
+    <>
+      <StatusBar/>
+      <Home/>
+    </>
+  );
+}
+
